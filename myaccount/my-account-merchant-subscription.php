48c70c4d1438966d5a862cf82579dc25394b2f97
kuldeep89 <d.kuldeepssb@gmail.com>
2015-06-16 22:03:30 +0530

    remove files and folder

--- myaccount/my-account-merchant-subscription.php
+++
@@ -1,2 +0,0 @@
-<h2>My Subscriptions</h2>
-<p>To manage your subscriptions, please call (574)269-0792 or email us at support@saltsha.com</p>
--- qb_api/app_ipp_v3/menu.php
+++
@@ -1,6 +0,0 @@
-<?php
-//25may2015: Chetu: use this when you need to require intuit menu
-require_once dirname(__FILE__) . '/config.php';
-
-// Display the menu
-die($IntuitAnywhere->widgetMenu($the_username, $the_tenant));
